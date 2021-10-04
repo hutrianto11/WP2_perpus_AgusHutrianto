63b5d015d3aaa07453b39480dbaf5de5906b1d26
halawa_oji97 <ozidhalawa97@gmail.com>
2021-10-04 19:03:18 +0700

    mengubah halaman about

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,18 +2,18 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : Imam</li>
-        <li>Nama Belakang : Nawawi</li>
+        <li>Nama Depan : Oji</li>
+        <li>Nama Belakang : Halawa</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+        <li> Kelapa Gading, Jakarta Utara</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
     <ul type="none">
-        <li>Cirebon</li>
+        <li>Nias, Medan, Sumatera Utara</li>
     </ul>
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
